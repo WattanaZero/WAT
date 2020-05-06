e01065250bbf81bb1e189f86144d2c24df8368d3
wattana chaiyakun <wattana-5695@hotmail.com>
2020-05-06 22:35:57 +0700

    -add repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
